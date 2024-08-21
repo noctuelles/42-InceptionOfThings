cfdda94e6adf18021cbcc55f42b20e36c8b99f29
noctuelles <plouvel@student.42.fr>
2024-08-21 18:06:36 +0200

    feat: ansible update

--- p1/scripts/bootstrap_server.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --token mypassword --write-kubeconfig-mode 644" sh -s -
--- p1/scripts/bootstrap_worker.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://192.168.56.110:6443 --token mypassword" sh -s -
