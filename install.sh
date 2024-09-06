4b191570531f42bab771757491119f16533d5465
noctuelles <plouvel@student.42.fr>
2024-09-06 17:57:22 +0200

    fix: activate virtual env in install script

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 echo -n "Creating virtual environment and installing requirements..."
 python3 -m venv .venv > /dev/null
+source .venv/bin/activate
 pip install -r requirements.txt > /dev/null
 echo " done."
 echo "Please type 'source .venv/bin/activate' to activate the virtual environment and begin to explore the project !"
